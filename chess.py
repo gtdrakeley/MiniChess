3d8ed0476c5d9eecf7c9db53c75437f13ca83539
Graham Drakeley <graham@debian.gtd-vm.com>
2016-04-03 02:07:28 -0700

    Added type suggestions to hw1 functions Initial implementation of hw1 functions:     reset     board*     winner     is*

--- chess.py
+++ chess.py
@@ -6,44 +6,67 @@
 import random
 
 
+board = ['.' * 5 for _ in range(6)]
+
+
 def reset():
     # reset the state of the game / your internal variables - note that this function is highly dependent on your
     # implementation
 
-    pass
+    global board
+
+    board = ['1 W',
+             'kqbnr',
+             'ppppp',
+             '.....'
+             '.....'
+             'PPPPP',
+             'RNBQK']
 
 
 def board_get():
     # return the state of the game - one example is given below - note that the state has exactly 40 or 41 characters
 
-    str_out = ''
+    global board
 
-    str_out += '1 W\n'
-    str_out += 'kqbnr\n'
-    str_out += 'ppppp\n'
-    str_out += '.....\n' * 2
-    str_out += 'PPPPP\n'
-    str_out += 'RNBQK\n'
-
-    return str_out
+    return '\n'.join(board) + '\n'
 
 
-def board_set(str_in):
+def board_set(str_in: str):
     # read the state of the game from the provided argument and set your internal variables accordingly - note that the
     # state has exactly 40 or 41 characters
 
-    pass
+    global board
+
+    board = str_in.split('\n')
+    board.remove('')
 
 
 def winner():
     # determine the winner of the current states of the game and return '?' or '=' or 'W' or 'B' - note that we are
     # returning a character and not a string
-    
-    return '?'
-    pass
+
+    global board
+
+    white_has_king, black_has_king = False, False
+
+    for row in board[1:]:
+        if 'k' in row:
+            black_has_king = True
+        if 'K' in row:
+            white_has_king = True
+
+    if '41' in board[0]:
+        return '='
+    elif white_has_king and black_has_king:
+        return '?'
+    elif white_has_king and not black_has_king:
+        return 'W'
+    else:
+        return 'B'
 
 
-def is_valid(int_x, int_y):
+def is_valid(int_x: int, int_y: int):
     if int_x < 0:
         return False
 
@@ -59,25 +82,42 @@ def is_valid(int_x, int_y):
     return True
 
 
-def is_enemy(str_piece):
+def is_enemy(str_piece: str):
     # with reference to the state of the game, return whether the provided argument is a piece from the side not on
     # move - note that we could but should not use the other is() functions in here but probably
 
-    return False
+    if 'W' in board[0] and str_piece.islower():
+        return True
+    elif 'W' in board[0] and str_piece.isupper():
+        return False
+    elif 'B' in board[0] and str_piece.isupper():
+        return True
+    else:
+        return False
 
 
-def is_own(str_piece):
+def is_own(str_piece: str):
     # with reference to the state of the game, return whether the provided argument is a piece from the side on move -
     # note that we could but should not use the other is() functions in here but probably
 
-    return False
+    if 'W' in board[0] and str_piece.isupper():
+        return True
+    elif 'W' in board[0] and str_piece.islower():
+        return False
+    elif 'B' in board[0] and str_piece.islower():
+        return True
+    else:
+        return False
 
 
-def is_nothing(str_piece):
+def is_nothing(str_piece: str):
     # return whether the provided argument is not a piece / is an empty field - note that we could but should not use
     # the other is() functions in here but probably
 
-    return False
+    if str_piece == '.':
+        return True
+    else:
+        return False
 
 
 def eval():
