e82c330ae4b80ed2a1be6931f2c5f7d51e0b8637
Graham Drakeley <graham@debian.gtd-vm.com>
2016-04-19 01:32:13 -0700

    Fixes for ChessAI.is_enemy and ChessAI.is_own Testing code for fixing ChessAI.winner

--- chessai.py
+++ chessai.py
@@ -3,8 +3,8 @@ import textwrap
 
 
 class ChessAI:
-    white_pieces = 'KQBNR'
-    black_pieces = 'kqbnr'
+    white_pieces = 'KQBNRP'
+    black_pieces = 'kqbnrp'
     piece_values = {'k': 10000, 'K': 10000,
                     'q': 5000, 'Q': 5000,
                     'b': 3000, 'B': 3000,
@@ -23,24 +23,40 @@ class ChessAI:
                       bytearray(b'RNBQK')]
 
     def reset(self):
-        self.__init__()
+        self.turn = 1
+        self.playing = 'W'
+        self.board = [bytearray(b'kqbnr'),
+                      bytearray(b'ppppp'),
+                      bytearray(b'.....'),
+                      bytearray(b'.....'),
+                      bytearray(b'PPPPP'),
+                      bytearray(b'RNBQK')]
 
     def board_get(self):
         return '{} {}\n{}\n'.format(self.turn, self.playing, '\n'.join(map(bytearray.decode, self.board)))
 
     def board_set(self, board_str):
+        print('board_set:')
+        for row in board_str.split('\n'):
+            print('\t' + row)
         turn_state, *board_state = board_str.split('\n')
         turn, playing = turn_state.split()
         self.turn, self.playing = int(turn), playing
         self.board = [bytearray(row.encode()) for row in board_state if len(row) > 0]
+        print('\tafter:')
+        for row in self.board:
+            print('\t\t' + row.decode())
 
     def winner(self):
-        board_concat = bytearray.join(bytearray(b''), self.board)
-        if ord('k') not in board_concat and ord('K') not in board_concat:
+        print('winner: {} {}'.format(self.playing, self.turn))
+        for row in self.board:
+            print('\t{}'.format(row.decode()))
+        board_concat = bytearray.join(bytearray(b''), self.board).decode()
+        if 'k' not in board_concat and 'K' not in board_concat:
             return '='
-        elif ord('k') in board_concat:
+        elif 'k' in board_concat:
             return 'W'
-        elif ord('K') in board_concat:
+        elif 'K' in board_concat:
             return 'B'
         elif self.turn > 40:
             return '='
@@ -61,6 +77,7 @@ class ChessAI:
             return True
 
     def is_enemy(self, str_piece: str):
+        print('is_enemy: {} {}'.format(self.playing, str_piece))
         if self.playing == 'W' and str_piece in ChessAI.black_pieces:
             return True
         elif self.playing == 'B' and str_piece in ChessAI.white_pieces:
@@ -69,6 +86,7 @@ class ChessAI:
             return False
 
     def is_own(self, str_piece: str):
+        print('is_own: {} {}'.format(self.playing, str_piece))
         if self.playing == 'W' and str_piece in ChessAI.white_pieces:
             return True
         elif self.playing == 'B' and str_piece in ChessAI.black_pieces:
