2cbfc2ca30ea42ab31721ced2248ed4275b1374e
gt.drakeley@gmail.com <hw00xl_56851>
2016-06-06 01:55:36 -0700

    Cleaned up main.py, uncommented execution of 0MQ

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 # CHANGE - Python 3 does not have an 'execfile' function, so we use imports instead
-import chess
 import zeromq
 
 
@@ -17,25 +16,7 @@ if __name__ == '__main__':
 
     random.seed()
 
-    # '''
-    from time import sleep
-    from chessai import ChessAI
-    ai = ChessAI()
-    while ai.winner() == '?':
-        # ai.move_random()
-        print('Eval: {}'.format(ai.eval()))
-        print('Board: {}'.format(ai))
-        print('Move: {}'.format(ai.move_negamax(3, 0)))
-        if ai.winner() != '?':
-            break
-        print('Eval: {}'.format(ai.eval()))
-        print('Board: {}'.format(ai))
-        print('Move: {}'.format(ai.move_alphabeta(5, 0)))
-        # ai.move_greedy()
-    print(ai.winner())
-    # '''
-
     # CHANGE - Calling a function from an imported file that is going to use globals located in the 'importer' would
     # require circular imports, so instead we pass them as function parameters
-    # zeromq.start(main_int_zeromq, main_str_name)
+    zeromq.start(main_int_zeromq, main_str_name)
 
