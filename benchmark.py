18babf08d476c82834d4cff1bde5f3bde3bb092c
gt.drakeley@gmail.com <hw00xl_56851>
2016-06-07 16:19:22 -0700

    Added modifiable depth to benchmark

--- benchmark.py
+++ benchmark.py
@@ -3,6 +3,7 @@ from sys import argv
 
 
 n = int(eval(argv[1]))
+depth = int(eval(argv[2]))
 setup1 = '''
 import chessai
 ai = chessai.ChessAI()
@@ -15,17 +16,17 @@ setup3 = '''
 import inline
 '''
 loop1 = '''
-ai.move_alphabeta(5, 0)
+ai.move_alphabeta(depth, 0)
 if ai.winner() != '?':
  ai.reset()
- '''
+ '''.replace('depth', str(depth))
 loop2 = '''
-inline.move_alphabeta(5, 0)
+inline.move_alphabeta(depth, 0)
 if inline.winner() != '?':
  inline.reset()
- '''
+ '''.replace('depth', str(depth))
 
-print('Benchmarking {} moves ({} games max)...'.format(n, n/80))
+print('Benchmarking {} moves with depth {} ({} games max)...'.format(n, depth, n/80))
 print('\tChessAI: {} seconds'.format(timeit(loop1, setup=setup1, number=n)))
 print('\tAI: {} seconds'.format(timeit(loop1, setup=setup2, number=n)))
 print('\tInline: {} seconds'.format(timeit(loop2, setup=setup3, number=n)))
