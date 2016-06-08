3997fd5729e7d23b80293def5c87f036166e064d
gt.drakeley@gmail.com <hw00xl_56851>
2016-06-07 23:38:53 -0700

    Mock tournament ready for testing

---
+++ tournament.py
@@ -0,0 +1,78 @@
+import chessai
+import inline
+from random import random
+
+
+p1_time = p2_time = 300000
+ai = chessai.ChessAI()
+
+if random() <= .5:
+    print('ChessAI is W')
+    while True:
+        start = inline.milliseconds()
+        ai.move_alphabeta(-1, p1_time)
+        p1_time -= inline.milliseconds() - start
+        print(ai.board_get())
+        print()
+        print()
+        if p1_time < 0:
+            print('B wins via time!')
+            break
+        elif ai.winner() == '=':
+            print('Tie game!')
+            break
+        elif ai.winner != '?':
+            print('W wins by taking the king!')
+            break
+        inline.set_board(ai.board_get())
+        start = inline.milliseconds()
+        inline.move_alphabeta(-1, p2_time)
+        p2_time -= inline.milliseconds() - start
+        print(inline.get_board())
+        print()
+        print()
+        if p2_time < 0:
+            print('W wins via time!')
+            break
+        elif inline.winner() == '=':
+            print('Tie game!')
+            break
+        elif inline.winner() != '?':
+            print('B wins by taking the king!')
+            break
+        ai.board_set(inline.get_board())
+else:
+    print('Inline is W')
+    while True:
+        start = inline.milliseconds()
+        inline.move_alphabeta(-1, p2_time)
+        p2_time -= inline.milliseconds() - start
+        print(inline.get_board())
+        print()
+        print()
+        if p2_time < 0:
+            print('W wins via time!')
+            break
+        elif inline.winner() == '=':
+            print('Tie game!')
+            break
+        elif inline.winner() != '?':
+            print('B wins by taking the king!')
+            break
+        ai.board_set(inline.get_board())
+        start = inline.milliseconds()
+        ai.move_alphabeta(-1, p1_time)
+        p1_time -= inline.milliseconds() - start
+        print(ai.board_get())
+        print()
+        print()
+        if p1_time < 0:
+            print('B wins via time!')
+            break
+        elif ai.winner() == '=':
+            print('Tie game!')
+            break
+        elif ai.winner != '?':
+            print('W wins by taking the king!')
+            break
+        inline.set_board(ai.board_get())
