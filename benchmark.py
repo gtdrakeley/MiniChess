4a2c47171deed954bf57c8427e77de2270f3d88a
gt.drakeley@gmail.com <hw00xl_56851>
2016-06-08 21:21:11 -0700

    Updated benchmarking to use ChessAIV2

--- benchmark.py
+++ benchmark.py
@@ -15,18 +15,23 @@ ai = cai.AI()
 setup3 = '''
 import inline
 '''
+setup4 = '''
+import chessaiv2
+ai = chessaiv2.ChessAIV2()
+'''
 loop1 = '''
 ai.move_alphabeta(depth, 0)
 if ai.winner() != '?':
  ai.reset()
- '''.replace('depth', str(depth))
+'''.replace('depth', str(depth))
 loop2 = '''
 inline.move_alphabeta(depth, 0)
 if inline.winner() != '?':
  inline.reset()
- '''.replace('depth', str(depth))
+'''.replace('depth', str(depth))
 
 print('Benchmarking {} moves with depth {} ({} games max)...'.format(n, depth, n/80))
 print('\tChessAI: {} seconds'.format(timeit(loop1, setup=setup1, number=n)))
 print('\tAI: {} seconds'.format(timeit(loop1, setup=setup2, number=n)))
 print('\tInline: {} seconds'.format(timeit(loop2, setup=setup3, number=n)))
+print('\tChessAIV2: {} seconds'.format(timeit(loop1, setup=setup4, number=n)))
