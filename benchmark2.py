934102b1f52c32c5c4f4cdb1cc7dd1b81ba4be66
gt.drakeley@gmail.com <hw00xl_56851>
2016-06-09 18:56:35 -0700

    Bug fixes to benchmark2

--- benchmark2.py
+++ benchmark2.py
@@ -17,28 +17,26 @@ ai = cai.AI()'''
 setup4 = '''
 import chess'''
 
-print('Generating random board state...', end='')
+print('Generating random board state...')
 ai = chessaiv2.ChessAIV2()
 while ai.turn < 20:
     ai.move_alphabeta(5, 0)
     if ai.winner() != '?':
         ai.reset()
 board = ai.get_board()
-print('Done')
 loop1 = '''
 ai.set_board(brd)
-ai.move_alphabeta(depth, 0)'''.replace('brd', board).replace('depth', str(depth))
+ai.move_alphabeta(depth, 0)'''.replace('brd', str(board)).replace('depth', str(depth))
 loop2 = '''
 chess.set_board(brd)
-chess.move_alphabeta(depth, 0)'''.replace('brd', board).replace('depth', str(depth))
-print('Calculating time required to set the board for each AI...', end='')
-chessai_set_time = timeit('ai.set_board(brd)'.replace('brd', board), setup=setup1, number=n)
-chessaiv2_set_time = timeit('ai.set_board(brd)'.replace('brd', board), setup=setup2, number=n)
-ai_set_time = timeit('ai.set_board(brd)'.replace('brd', board), setup=setup3, number=n)
-chess_set_time = timeit('chess.set_board(brd)'.replace('brd', board), setup=setup4, number=n)
-print('Done')
+chess.move_alphabeta(depth, 0)'''.replace('brd', str(board)).replace('depth', str(depth))
+print('Calculating time required to set the board for each AI...')
+chessai_set_time = timeit('ai.set_board(brd)'.replace('brd', str(board)), setup=setup1, number=n)
+chessaiv2_set_time = timeit('ai.set_board(brd)'.replace('brd', str(board)), setup=setup2, number=n)
+ai_set_time = timeit('ai.set_board(brd)'.replace('brd', str(board)), setup=setup3, number=n)
+chess_set_time = timeit('chess.set_board(brd)'.replace('brd', str(board)), setup=setup4, number=n)
 print('Benchmarking {} moves with depth {} ({} games max)...'.format(n, depth, n/80))
-print('\tChessAI: {} seconds'.format(timeit(loop1, setup=setup1, number=n)-chess_set_time))
+print('\tChessAI: {} seconds'.format(timeit(loop1, setup=setup1, number=n)-chessai_set_time))
 print('\tChessAIV2: {} seconds'.format(timeit(loop1, setup=setup2, number=n)-chessaiv2_set_time))
 print('\tAI: {} seconds'.format(timeit(loop1, setup=setup3, number=n)-ai_set_time))
 print('\tChess: {} seconds'.format(timeit(loop2, setup=setup4, number=n)-chess_set_time))
