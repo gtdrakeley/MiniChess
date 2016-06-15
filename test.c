50158265a9e31bead612b26fe42509d43e1e4441
Graham Drakeley <drak2@white-flipper.cs.pdx.edu>
2016-06-15 03:49:08 -0700

    Changed test tournament to use timed AIs

--- test.c
+++ test.c
@@ -8,6 +8,9 @@
 
 
 int main(int argc, char** argv) {
+	int w_time;
+	int b_time;
+	unsigned long long start;
     int w_wins = 0;
     int b_wins = 0;
     int ties = 0;
@@ -17,25 +20,35 @@ int main(int argc, char** argv) {
     ChessAI_init(&white);
     ChessAI_init(&black);
 
-    while ((w_wins + b_wins + ties) < 100) {
+    while ((w_wins + b_wins + ties) < 10) {
         ChessAI_reset(&white);
         ChessAI_reset(&black);
+		w_time = 300000;
+		b_time = 300000;
         while (true) {
-            ChessAI_moveAlphabeta(&white, 4, 0);
+			start = msec();
+            ChessAI_moveAlphabeta(&white, -1, w_time);
+			w_time -= msec() - start;
             ChessAI_sync(&white, &black);
-            if (ChessAI_winner(&white) != '?') { break; }
-            ChessAI_moveAlphabeta(&black, -1, 2000);
+            if (ChessAI_winner(&white) != '?' || w_time <= 0) { break; }
+			start = msec();
+            ChessAI_moveAlphabeta(&black, -1, b_time);
+			b_time -= msec() - start;
             ChessAI_sync(&black, &white);
-            if (ChessAI_winner(&black) != '?') { break; }
+            if (ChessAI_winner(&black) != '?' || b_time <= 0) { break; }
         }
         if (ChessAI_winner(&white) == 'W') {
             ++w_wins;
-        } else if (ChessAI_winner(&white) == 'B') {
+        } else if (ChessAI_winner(&white) == 'B' ) {
             ++b_wins;
         } else if (ChessAI_winner(&white) == '=') {
             ++ties;
+		} else if (w_time <= 0) {
+			++b_wins;
+		} else if (b_time <= 0) {
+			++w_wins;
         } else {
-            sentry("Game exitied with winner \"?\"");
+            sentry("Game exited with winner \"?\"");
         }
     
         printf("%d : %d : %d\n", w_wins, b_wins, ties);
